5fc5db2389f40e99a52f3b8e03cfacca7c6c20ff
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-01-22 23:49:26 -0500

    :heavy_plus_sign: feat: talwind install

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Rutas de tus archivos de código fuente
+    "./public/index.html", // Ruta de tu archivo HTML principal
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
